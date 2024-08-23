04ee0fff6a0b689de3deb7f8e8d12b1e8acd9542
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-08-24 01:11:34 +0400

    git init made basic structure for the exam 10 back and front

---
+++ back_exam/mysqlDb.ts
@@ -0,0 +1,16 @@
+import mysql, {Connection} from "mysql2/promise";
+import config from "./config";
+
+let connection:Connection
+
+const mysqlDB ={
+    init:async ()=>{
+        connection = await mysql.createConnection(config.database)
+    },
+
+    getConnection: ()=>{
+        return connection;
+    }
+}
+
+export default mysqlDB;
---
+++ front_exam/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Ready for the exam 10</h1>
+    </>
+  )
+};
+
+export default App
